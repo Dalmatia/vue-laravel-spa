95fba45ae1c7b5ba70a4f177dc27098609350e7b
高原健 <takaharaken@takaharakennoMacBook-Air.local>
2023-08-18 06:19:36 +0900

    Modify the home page and create a display of the post overlay

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -24,7 +24,135 @@ onMounted(() => {
 <template>
     <div id="Posts" class="md:pr-1.5 lg:pl-0 md:pl-[90px]">
         <div class="grid md:gap-4 gap-1 grid-cols-3 relative">
-            <ShowPostOverlay />
+            <article class="overflow-hidden rounded-lg shadow-lg">
+                <a @click="openOverlay = true">
+                    <img
+                        class="block h-[193px] w-[177px] md:h-auto md:w-full"
+                        src="https://picsum.photos/id/600/480/?random"
+                    />
+                </a>
+                <p class="text-grey-darker text-sm">11/1/19</p>
+                <footer
+                    class="flex items-center justify-between leading-none mt-3 md:p-4"
+                >
+                    <a
+                        class="flex items-center no-underline hover:underline text-black"
+                        href="#"
+                    >
+                        <img
+                            alt="Placeholder"
+                            class="block rounded-full"
+                            src="https://picsum.photos/id/32/32/32"
+                        />
+                        <p class="ml-2 text-xs">Author Name</p>
+                    </a>
+                    <a
+                        href="#"
+                        class="float-right bg-blue-500 py-1 text-white font-semibold text-sm rounded text-center hidden md:inline-block"
+                    >
+                        フォロー
+                    </a>
+                </footer>
+            </article>
+
+            <article class="overflow-hidden rounded-lg shadow-lg">
+                <a href="/">
+                    <img
+                        class="block h-[193px] w-[177px] md:h-auto md:w-full"
+                        src="https://picsum.photos/id/600/480/?random"
+                    />
+                </a>
+                <p class="text-grey-darker text-sm">11/1/19</p>
+                <footer
+                    class="flex items-center justify-between leading-none mt-3 md:p-4"
+                >
+                    <a
+                        class="flex items-center no-underline hover:underline text-black"
+                        href="#"
+                    >
+                        <img
+                            alt="Placeholder"
+                            class="block rounded-full"
+                            src="https://picsum.photos/id/32/32/32"
+                        />
+                        <p class="ml-2 text-xs">Author Name</p>
+                    </a>
+                    <a
+                        href="#"
+                        class="float-right bg-blue-500 py-1 text-white font-semibold text-sm rounded text-center hidden md:inline-block"
+                    >
+                        フォロー
+                    </a>
+                </footer>
+            </article>
+
+            <article class="overflow-hidden rounded-lg shadow-lg">
+                <a href="/">
+                    <img
+                        class="block h-[193px] w-[177px] md:h-auto md:w-full"
+                        src="https://picsum.photos/id/600/480/?random"
+                    />
+                </a>
+                <p class="text-grey-darker text-sm">11/1/19</p>
+                <footer
+                    class="flex items-center justify-between leading-none mt-3 md:p-4"
+                >
+                    <a
+                        class="flex items-center no-underline hover:underline text-black"
+                        href="#"
+                    >
+                        <img
+                            alt="Placeholder"
+                            class="block rounded-full"
+                            src="https://picsum.photos/id/32/32/32"
+                        />
+                        <p class="ml-2 text-xs">Author Name</p>
+                    </a>
+                    <a
+                        href="#"
+                        class="float-right bg-blue-500 py-1 text-white font-semibold text-sm rounded text-center hidden md:inline-block"
+                    >
+                        フォロー
+                    </a>
+                </footer>
+            </article>
+
+            <article class="overflow-hidden rounded-lg shadow-lg">
+                <a href="/">
+                    <img
+                        class="block h-[193px] w-[177px] md:h-auto md:w-full"
+                        src="https://picsum.photos/id/600/480/?random"
+                    />
+                </a>
+                <p class="text-grey-darker text-sm">11/1/19</p>
+                <footer
+                    class="flex items-center justify-between leading-none mt-3 md:p-4"
+                >
+                    <a
+                        class="flex items-center no-underline hover:underline text-black"
+                        href="#"
+                    >
+                        <img
+                            alt="Placeholder"
+                            class="block rounded-full"
+                            src="https://picsum.photos/id/32/32/32"
+                        />
+                        <p class="ml-2 text-xs">Author Name</p>
+                    </a>
+                    <a
+                        href="#"
+                        class="float-right bg-blue-500 py-1 text-white font-semibold text-sm rounded text-center hidden md:inline-block"
+                    >
+                        フォロー
+                    </a>
+                </footer>
+            </article>
         </div>
     </div>
+
+    <ShowPostOverlay
+        v-if="openOverlay"
+        :post="currentPost"
+        @closeOverlay="openOverlay = false"
+    />
 </template>
