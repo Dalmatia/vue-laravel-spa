e15eadff97cfd14c416e4b033e3ad5515309b713
高原健 <takaharaken@takaharakennoMacBook-Air.local>
2024-01-20 19:15:28 +0900

    Corrected handling of mouse events and actions when selecting posted outfits on user pages.

--- resources/js/Components/ContentOverlay.vue
+++ resources/js/Components/ContentOverlay.vue
@@ -8,12 +8,12 @@ import ShowOutfitOverlay from './ShowOutfitOverlay.vue';
 import Heart from 'vue-material-design-icons/Heart.vue';
 import Comment from 'vue-material-design-icons/Comment.vue';
 
-let isHover = ref(false);
 let currentOutfit = ref(null);
 let openOverlay = ref(false);
 
 const authStore = useAuthStore();
 const outfits = ref([]);
+const isHover = ref(Array(outfits.value.length).fill(false));
 
 const fetchOutfits = async () => {
     try {
@@ -60,16 +60,16 @@ onUnmounted(() => {
 
 <template>
     <div
-        @mouseenter="isHover = true"
-        @mouseleave="isHover = false"
         class="flex items-center justify-center cursor-pointer relative"
-        v-for="outfit in outfits"
+        v-for="(outfit, index) in outfits"
         :key="outfit.id"
         @click="openOutfitOverlay(outfit)"
+        @mouseenter="isHover[index] = true"
+        @mouseleave="isHover[index] = false"
     >
         <div
-            v-if="isHover"
-            :class="isHover ? 'bg-black bg-opacity-40' : ''"
+            v-if="isHover[index]"
+            :class="isHover[index] ? 'bg-black bg-opacity-40' : ''"
             class="absolute w-full h-full z-50 flex items-center justify-around text-lg font-extrabold text-white"
         >
             <div class="flex items-center justify-around w-[50%]">
